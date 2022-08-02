1685337acbd1fce8770d87b16279a7771523f91d
songulates <songl.ates.02@gmail.com>
2022-08-02 14:51:25 +0300

    ersm

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+
+namespace erisim_metod 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         calisan calisan1=new calisan();
+         calisan1.ad="sa";
+         calisan1.yas=25;
+         calisan1.calisan_bilgileri();
+
+        }
+
+    }
+    class calisan //sınıf
+    {
+        public string ad;
+        public int yas;
+        
+        public void calisan_bilgileri()//metod
+        {
+
+            Console.WriteLine("ad: {0}",ad);
+             Console.WriteLine("ad: {0}",yas);
+        }
+    }
+}
+
